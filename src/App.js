6edc77447484a6f394c7586fd16c2be319af5446
Rodolfo Miguel de Sousa <rodolfo.de_sousa@siemens-energy.com>
2024-04-16 18:50:11 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
 import './App.css';
+import React from 'react';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
+import Root from 'pages/Root';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+			<ThemeProvider theme={theme}>
+        <div>
+          <Root></Root>
+        </div>
+      </ThemeProvider>
+		</Fragment>
   );
 }
 
---
+++ src/pages/Root/index.js
@@ -0,0 +1,10 @@
+import { Button } from '@mui/material';
+import React from 'react'
+
+const Root = () => {
+    return (
+        <Button variant='contained'>Button Contained</Button>
+    );
+}
+
+export default Root;
